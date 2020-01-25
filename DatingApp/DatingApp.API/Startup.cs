1ae0709ed23c80a1fd288d180ed653784b8d9842
Robin Haider <robinhaider69@gmail.com>
2020-01-25 22:05:04 +0600

    Build an app with ASPNET Core and Angular from scratch - DatingApp - Day Two - 4 - CORS support, frontend can now talk to api and display the data

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(options => 
                     options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseRouting();
 
